448f71af21eb780234d17786a9d827a035f1f256
Krivoruchko <georgy.krivoruchko@intel.com>
2024-04-30 16:28:36 +0400

    Added NPU for future usage

---
+++ runtimes/openvino/npu_stable/collect.bat
@@ -0,0 +1,65 @@
+setlocal
+IF "%SCROOT%" == "" (
+    call :changescroot
+)
+IF NOT EXIST %SCROOT%\windows\venv\Scripts\activate.bat (
+    @echo Python's VirtualENV isn't found, run %SCROOT%\windows\03.Setup.VENV.bat first
+    goto :eof
+)
+call %SCROOT%\windows\venv\Scripts\activate.bat
+
+IF EXIST %SCROOT%\windows\CustomVars.bat (
+    call %SCROOT%\windows\CustomVars.bat
+)
+
+REM Session variables START
+set TEST_DEVICE=NPU
+set PYTHONPATH=%SCROOT%\windows\openvino\src\frontends\onnx\;%PYTHONPATH%
+set RESULTS_DIR=%SCROOT%\results\openvino\npu_stable
+set CSVDIR=%SCROOT%\results\openvino\npu_stable
+REM Comment line below if you would use manually built OpenVINO
+set OV_WHEELS=%OV_STABLE%
+REM Session variables END
+
+REM Environment check START
+IF "%OV_WHEELS%" == "" (
+    where openvino.dll
+    IF NOT "%ERRORLEVEL%" == "0" (
+        @echo Check availability of openvino.dll using PATH environment variable
+        @echo Check CustomVars.bat
+        goto final
+    )
+) ELSE (
+    pip install --no-cache-dir %OV_WHEELS%
+)
+python -c "import openvino;print(openvino.runtime.get_version());"
+IF NOT "%ERRORLEVEL%" == "0" (
+    @echo Cannot import OpenVINO Python module, check PYTHONPATH in %0
+    goto final
+)
+IF NOT EXIST "%RESULTS_DIR%" (
+    mkdir "%RESULTS_DIR%
+)
+REM Environment check END
+
+REM WORKLOAD START
+pushd
+cd %SCROOT%\windows
+python -c "import re;txt='';f=open('openvino/src/frontends/onnx/tests/__init__.py','r');txt=re.sub(r'BACKEND_NAME = ""?[0-9A-Za-z\.]+""?','BACKEND_NAME = ""%TEST_DEVICE%""',f.read());f.close();f=open('openvino/src/frontends/onnx/tests/__init__.py','w');f.write(txt);f.close()"
+popd
+pip list --format=json > %RESULTS_DIR%/pip-list.json
+pytest %SCROOT%\test\test_backend.py --onnx_backend=%ONNX_BACKEND%  -k "not _cuda" -v
+REM WORKLOAD END
+
+:final
+IF NOT "%OV_WHEELS%" == "" (
+    pip uninstall --y %OV_WHEELS%
+)
+call %SCROOT%\windows\venv\Scripts\deactivate.bat
+endlocal
+goto :eof
+
+:changescroot
+set SCROOT=%~p0..\..\..
+cd %SCROOT%\windows
+goto :eof
--- windows/CustomVars.bat
+++ windows/CustomVars.bat
@@ -5,5 +5,5 @@ REM set OPENVINO_LIB_PATHS=e:\openvino\ov_onnx\bin\intel64\RelWithDebInfo;
 REM set PYTHONPATH=e:\openvino\ov_onnx\bin\intel64\RelWithDebInfo\python;%PYTHONPATH%
 REM If you use pre-built wheels - comment variables above and set versions below
 REM You should download a wheels, put here names or full path to it
-set OV_STABLE=openvino-2023.3.0-13785-cp38-cp38-win_amd64.whl
-set OV_LATEST=openvino-2024.0.0.dev20240118-14040-cp38-cp38-win_amd64.whl
+set OV_STABLE=openvino-2024.1.0-15028-cp38-cp38-win_amd64.whl
+set OV_LATEST=openvino-2024.2.0.dev20240429-15210-cp38-cp38-win_amd64.whl
